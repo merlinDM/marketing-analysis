5df77d54c3c255ea98c2c32fe55cfd1991ec5230
Mikhail Davydov <mdavydov@griddynamics.com>
2020-01-17 13:54:28 +0300

    Add test for join() method.

--- src/test/scala/com/gd/AttributionNodeTest.scala
+++ src/test/scala/com/gd/AttributionNodeTest.scala
@@ -2,12 +2,11 @@ package com.gd
 
 import com.gd.model.{AttributionSource, EventSource, PurchaseSource}
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions._
 import org.scalatest.FunSuite
 
 class AttributionNodeTest extends FunSuite {
 
-  private val spark = SparkSession
+  SparkSession
     .builder()
     .appName("marketing-attribution-test")
     .master("local[*]")
@@ -15,11 +14,11 @@ class AttributionNodeTest extends FunSuite {
     .config("spark.local.dir", "/tmp/spark")
     .getOrCreate()
 
-  test("data samples") {
+  test("test samples") {
     val eventSource = new EventSource()
     val events = eventSource.read()
 
-    events.withColumn("campaignId", col("attributes.campainId")).show(100, truncate = false)
+    events.show(100, truncate = false)
 
     val purchaseSource = new PurchaseSource()
     val purchases = purchaseSource.read()
@@ -34,5 +33,22 @@ class AttributionNodeTest extends FunSuite {
 
   test("test Join method") {
 
+    val eventSource = new EventSource()
+    val events = eventSource.read()
+
+    val purchaseSource = new PurchaseSource()
+    val purchases = purchaseSource.read()
+
+    val attributionNode = new AttributionNode()
+    val attributions = attributionNode.join(events, purchases)
+
+    val attributionSource = new AttributionSource()
+    val expectedAttributions = attributionSource.read()
+
+    assert(attributions.schema.equals(expectedAttributions.schema),
+      "Resulted dataset should have the same schema as expected one.")
+
+    assert(attributions.exceptAll(expectedAttributions).isEmpty,
+      "Resulted dataset should be equivalent to the expected one.")
   }
 }
